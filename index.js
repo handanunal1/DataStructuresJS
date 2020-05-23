8fffe46aac5dbbd15e494f8759276a9aab2d1063
Handan Unal <handan.unal@yahoo.com>
2020-05-23 11:28:39 +0000

    collision prevention

--- index.js
+++ index.js
@@ -33,19 +33,45 @@ class HashTable {
     }
     return undefined;
   }
-  
-  keys(){
-    const keysArray = [];
- 
-    for (let i = 0; i < this.data.length; i++){
-      if(this.data[i]){
-        keysArray.push(this.data[i][0][0])
-      }
-    }
 
-    return keysArray;
+
+
+ keys() {
+    if (!this.data.length) {
+      return undefined
+    }
+    let result = []
+    // loop through all the elements
+    for (let i = 0; i < this.data.length; i++) {
+        // if it's not an empty memory cell
+        if (this.data[i] && this.data[i].length) {
+          // but also loop through all the potential collisions
+          if (this.data.length > 1) {
+            for (let j = 0; j < this.data[i].length; j++) {
+              result.push(this.data[i][j][0])
+            }
+          } else {
+            result.push(this.data[i][0])
+          } 
+        }
+    }
+    return result; 
   }
-}
+
+
+  
+//   keys(){
+//     const keysArray = [];
+ 
+//     for (let i = 0; i < this.data.length; i++){
+//       if(this.data[i]){
+//         keysArray.push(this.data[i][0][0])
+//       }
+//     }
+
+//     return keysArray;
+//   }
+// }
 
 const myHashTable = new HashTable(50);
 myHashTable.set('grapes', 10000);
